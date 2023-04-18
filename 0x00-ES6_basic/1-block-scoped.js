c3f72db97f58c429523d3a627c01e4ff7977172d
theeduke <oscarian528@gmail.com>
2023-04-17 22:53:15 -0700

    correction

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -2,8 +2,8 @@ export default funtion taskBlock(trurOrFalse){
 	const task = false;
 	const task2 = true;
 	if (trueOrFalse){
-		var task = true;
-		var task2 = false;
+		const task = true;
+		const task2 = false;
 	}
 	return [task, task2];
 }
