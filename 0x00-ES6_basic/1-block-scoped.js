bffbc63d1934b2e51bc69d4300d2fd020743ba63
theeduke <oscarian528@gmail.com>
2023-04-17 22:28:10 -0700

    task 2

---
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -0,0 +1,10 @@
+export default funtion taskBlock(trurOrFalse){
+	var task = false;
+	var task2 = true;
+	if (trueOrFalse){
+		var task = false;
+		var task2 = true;
+		return [task, task2]
+	}
+	return [task, task2]
+}
