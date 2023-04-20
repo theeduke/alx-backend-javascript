b292ad2bceaf19dfc10ce0a9ab90c04d7fd04e21
theeduke <oscarian528@gmail.com>
2023-04-20 01:31:42 -0700

    update

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -1,7 +1,7 @@
 export default function getNeighborhoodsList() {
   this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
 	const self = this;
-	this.addNeighborhood => function add(newNeighborhood) {
+	this.addNeighborhood = add(newNeighborhood) =>{
 		self.sanFranciscoNeighborhoods.push(newNeighborhood);
 	return self.sanFranciscoNeighborhoods;
   };
