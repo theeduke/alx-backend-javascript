65b185fb6c69a2af7e9f1f4bef5e7fd460e961a4
theeduke <oscarian528@gmail.com>
2023-04-20 16:22:27 -0700

    update on classes

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -2,9 +2,9 @@ import ClassRoom from './0-classroom';
 
 export default function initializeRooms(){
 	const arr = [
-	new ClassRoom(19),
-	new ClassRoom(20),
-	new ClassRoom(34),
+		new ClassRoom(19),
+		new ClassRoom(20),
+		new ClassRoom(34),
 	];
-	return (arr):
+	return (arr);
 }
