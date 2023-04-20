21ccefc681a912b97bf2106a925dded0bd0f57de
theeduke <oscarian528@gmail.com>
2023-04-20 01:55:10 -0700

    update

---
+++ 0x00-ES6_basic/2-main.js
@@ -0,0 +1,5 @@
+import getNeighborhoodsList from './2-arrow.js';
+
+const neighborhoodsList = new getNeighborhoodsList();
+const res = neighhborhoodsList.addNeighborhood('Noe Valley');
+console.log(res);
