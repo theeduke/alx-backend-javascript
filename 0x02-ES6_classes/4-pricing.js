aafe188f16892e235e67a48bf2132c91417e6f25
theeduke <oscarian528@gmail.com>
2023-04-20 17:21:07 -0700

    4-pricing.js

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,7 +1,30 @@
-import Currency from './3-currency.js'
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
 
-export default class Pricing{
-	constructor(amount, currency){
-		this.amount = amount;
-		this.currency = currency;
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(n) {
+    this._amount = n;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(n) {
+    this._currency = n;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
 }
