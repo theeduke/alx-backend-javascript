7716f014ebb2322580c6b1bf8cbe9cb83eaf0d10
theeduke <oscarian528@gmail.com>
2023-04-20 17:50:04 -0700

    7-airport.js

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -1,10 +1,10 @@
-export default class Airprort {
-	constructor(name, code) {
-		this._name = name;
-		this._code = code;
-	}
+export default class Airport {
+  constructor(name, code) {
+    this._name = name;
+    this._code = code;
+  }
 
-	toString() {
-		return '[object ${this._code}]';
-	}
+  toString() {
+    return `[object ${this._code}]`;
+  }
 }
