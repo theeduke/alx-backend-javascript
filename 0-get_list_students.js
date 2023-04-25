2008428d08fd13a394351d28f529ecbe280d556f
theeduke <oscarian528@gmail.com>
2023-04-25 15:56:35 -0700

    update

--- 0-get_list_students.js
+++
@@ -1,6 +0,0 @@
-const getListStudents = () =>[
-	{id: 1, firstName: 'Guillaume', location: 'San Francisco'},
-	{id: 2, firstName: 'Jaames', location: 'Columbia'},
-	{id: 3, firstName: 'Serena', location: 'San Francisco'},
-];
-export default getListStudents;
