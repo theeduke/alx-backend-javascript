b77e015464b29b734fcaccf94d19b0ce8578c2f6
theeduke <oscarian528@gmail.com>
2023-04-25 15:25:23 -0700

    0-get_list_students.js

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,7 +1,7 @@
-const getListStudents = () =>[
-	{id: 1, firstName: 'Guillaume', location: 'San Francisco'},
-	{id: 2, firstName: 'Jaames', location: 'Columbia'},
-	{id: 3, firstName: 'Serena', location: 'San Francisco'},
+const getListStudents = () => [
+  { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+  { id: 2, firstName: 'James', location: 'Columbia' },
+  { id: 5, firstName: 'Serena', location: 'San Francisco' },
 ];
 
 export default getListStudents;
