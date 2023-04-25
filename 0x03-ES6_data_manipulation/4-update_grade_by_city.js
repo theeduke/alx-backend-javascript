9cea83b8158fe10b1667b04c3ef73c7e758defce
theeduke <oscarian528@gmail.com>
2023-04-25 15:46:39 -0700

    update_grade_by_city.js

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,12 @@
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  if (!Array.isArray(students) || !Array.isArray(newGrades)) {
+    return [];
+  }
+
+  const cityStudents = students.filter((student) => student.location === city).map((student) => {
+    const item2 = newGrades.find((student2) => student.id === student2.studentId);
+
+    return { ...student, grade: item2 ? item2.grade : 'N/A' };
+  });
+  return cityStudents;
+}
